e7afe95936c257b3b8995d76abcc2c3e9f803d7f
7westdesign <sevenwestdesign@gmail.com>
2015-02-10 10:55:53 -0800

    ready for frame

--- email_us/mailer.php
+++ email_us/mailer.php
@@ -1,6 +1,6 @@
 <?php
 /* Set e-mail recipient */
-$myemail = "westaraluce@gmail.com";
+$myemail = "karilisak@gmail.com";
 
 /* Check all form inputs using check_input function */
 $name = check_input($_POST['name'], "Your Name");
